083bc62ab02b11fb03fe9f04dd29ccf6a4c52750
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-22 11:34:15 -0300

    :wrench: Alterando imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./components/main/App";
+import "./index.scss";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
