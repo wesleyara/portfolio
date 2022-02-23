1d089aa8636f826b1f2ab0d487ac82ce8624508d
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-02-23 17:00:24 -0300

    :wrench: Criando página notfound

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import err from "../Assets/Images/Error_404_SVG.svg";
+
+export default function NotFound() {
+  return (
+    <div className="containerError">
+      <div className="bxImageError">
+        <Image src={err} alt="" />
+      </div>
+      <br />
+      <br />
+      <p>
+        Ooooops... a página que você está acessando não existe ou não terminou
+        de ser produzida.
+      </p>
+      <p className="linkError">
+        Retorne para <Link href="/">Home page</Link>.
+      </p>
+    </div>
+  );
+}
