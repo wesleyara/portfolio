2d85c11b9c285651d6abdc2bb3bbd43e649d2566
wesleyara <wes1gom@gmail.com>
2022-03-29 14:04:19 -0300

    style: Alterando as imagens

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -63,7 +63,7 @@ export default function Projects() {
 
           <ProjectsBox
             title="Calculadora de Preço Médio Alvo"
-            link="https://github.com/wesleyara/rest-countries-api"
+            link="https://github.com/wesleyara/calculadora-de-pm-alvo-para-acoes"
             src={calculadora}
             img1="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
             img2="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
@@ -71,7 +71,7 @@ export default function Projects() {
 
           <ProjectsBox
             title="Página de Captura"
-            link="https://github.com/wesleyara/rest-countries-api"
+            link="https://github.com/wesleyara/pagina-de-captura-para-curso"
             src={paginaCaptura}
             img1="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
             img2="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white"
