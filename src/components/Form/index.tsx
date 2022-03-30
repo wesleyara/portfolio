f49a429d66afa97b4636f89a96d799dfb497bf55
wesleyara <wes1gom@gmail.com>
2022-03-30 13:43:11 -0300

    feat: Criando form de contato

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,26 @@
+import { FormEvent } from "react";
+import { Container } from "./style";
+
+export function Form() {
+  function handleMessageSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
+  return (
+    <Container data-aos="fade-left" onSubmit={handleMessageSubmit}>
+      <h2>Entre em contato comigo!</h2>
+      <label htmlFor="name">Nome:</label>
+      <input type="text" id="name" placeholder="Seu Nome" />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" placeholder="Seu Email" />
+      <label htmlFor="message">Mensagem:</label>
+      <textarea
+        id="message"
+        placeholder="Escreva uma mensagem"
+        cols={30}
+        rows={10}
+      ></textarea>
+      <button type="submit">Enviar</button>
+    </Container>
+  );
+}
