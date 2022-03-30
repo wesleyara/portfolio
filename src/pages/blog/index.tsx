5b42ee0bcddb753eec96a6a70ff508f9e4db6d94
wesleyara <wes1gom@gmail.com>
2022-03-30 16:57:48 -0300

    fix: Corrigindo bug

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,10 +1,8 @@
 import { Footer } from "../../layout/Footer";
-import { Header } from "../../layout/Header";
 
 export default function Blog() {
   return (
     <div>
-      <Header />
       <Footer />
     </div>
   );
