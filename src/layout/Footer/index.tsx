d58018e8c23509cda154c87e5876f1170c6bda3a
wesleyara <wes1gom@gmail.com>
2022-04-11 21:33:27 -0300

    feat: Alterando estilo do footer

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,13 +1,31 @@
 import { Container } from "./style";
+import Link from "next/link";
 
 export function Footer() {
   return (
     <Container>
-      <a className="copyright" href="mailto:wesleyara.contato@gmail.com">
-        <p>Desenvolvido por Wesley Araújo</p>
-      </a>
-      <br />
-      <p className="reserv">Todos os direitos reservados - Wesley Araújo</p>
+      <div>
+        <a className="copyright" href="mailto:wesleyara.contato@gmail.com">
+          <p>Desenvolvido por Wesley Araújo</p>
+        </a>
+        <br />
+        <p className="reserv">
+          2022&copy; Todos os direitos reservados - Wesley Araújo
+        </p>
+      </div>
+      <nav>
+        <ul>
+          <Link href="/blog">
+            <li>Blog</li>
+          </Link>
+          <Link href="/">
+            <li>Portfólio</li>
+          </Link>
+          <Link href="/github">
+            <li>Github</li>
+          </Link>
+        </ul>
+      </nav>
     </Container>
   );
 }
--- src/layout/Footer/style.ts
+++ src/layout/Footer/style.ts
@@ -1,21 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
-  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  max-width: 1100px;
+  margin: 0 auto;
   padding: 30px 1rem;
-  border-top: 1px solid ${(props) => props.theme.colors.text};
-
-  .reserv {
-    font-size: 20px;
-  }
-
-  .copyright {
-    font-size: 20px;
-    color: ${(props) => props.theme.colors.text};
-  }
+  flex-wrap: wrap;
+  background-color: ${(props) => props.theme.colors.background};
 
   .copyright:hover {
     text-decoration: underline;
     color: ${(props) => props.theme.colors.text};
+  }
+
+  ul {
+    display: flex;
+
+    li {
+      padding: 20px;
+      cursor: pointer;
+      transition: 0.2s ease-in-out;
+
+      &:hover {
+        background-color: ${(props) => props.theme.colors.text};
+        color: ${(props) => props.theme.colors.background};
+      }
+    }
+  }
+
+  @media (max-width: 550px) {
+    justify-content: flex-start;
+
+    ul {
+      gap: 20px;
+      margin-top: 15px;
+
+      li {
+        padding: 0;
+      }
+    }
   }
 `;
